05b89936dd6386ec53ed2a5c169aeef11b054af3
Christos Plourchou <cploutarchou@gmail.com>
2020-06-24 00:56:09 +0300

    create memory class

--- app/memory.py
+++ app/memory.py
@@ -1,11 +1,22 @@
+from app.helper import *
 
-def get_memory_details():
-    memory_details = {}
-    with open("/proc/meminfo", "r") as file:
-        lines = file.readlines()
-        for row in lines:
-            row = row.strip()
-            row = row.split()
-            row = {row[0]: row[1]}
-            memory_details.update(row)
-    return memory_details
+
+class Memory:
+
+    def __init__(self):
+        info = self.get_memory_details()
+        for k, v in info.items():
+            print(k,v)
+            self.k = v
+
+    @staticmethod
+    def get_memory_details():
+        memory_details = {}
+        with open("/proc/meminfo", "r") as file:
+            lines = file.readlines()
+            for row in lines:
+                row = row.strip()
+                row = row.split()
+                row = {row[0]: row[1]}
+                memory_details.update(row)
+        return memory_details
